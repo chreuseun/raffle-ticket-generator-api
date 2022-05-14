7dfbad5b5c56626d9a92b5665a6370382d03f8c7
eunille <eunille.villegas@growsari.com>
2022-05-15 01:18:58 +0800

    done savePrintingRecord insert 1 record API

--- src/POST/index.js
+++ src/POST/index.js
@@ -1,3 +1,4 @@
 module.exports.createUser = require('./createUser')
 module.exports.updateUser = require('./updateUser')
 module.exports.loginUser  = require('./loginUser');
+module.exports.savePrintingRecord  = require('./savePrintingRecord');
---
+++ src/POST/savePrintingRecord.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const { printRecord }  = require('../schemas/printRecord')
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+
+const savePrintRecord =  async (printRecord, callback) => {
+  try{
+    console.log(`Saving Print Record in ${process.env.TABLE_PRINT_RECORDS}`);
+
+    const printRecordInfo = {
+      TableName: process.env.TABLE_PRINT_RECORDS,
+      Item: printRecord
+    }
+
+    await dynamoDb.put(printRecordInfo).promise();
+
+    callback(null,{
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Printing Record: Successfully saved.`,
+        success:true
+      })
+    });
+
+  }catch(err){
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Save Print Record: Database operation related issue, unable to save print record. Error details: ${err} `,
+        success: false
+      })
+    })
+  }
+}
+
+
+const savePrintingRecords = async ( event, _, callback ) => {
+  try{
+    const requestBody = JSON.parse(event.body);
+    const {
+      creator = '',
+      row_count = '',
+      initial_id = '',
+      prefix = ''
+    } = requestBody
+    const isValidated = [creator,row_count,initial_id,prefix].every(i => !!i);
+
+    if(isValidated){
+      await savePrintRecord(
+        printRecord({
+          creator,
+          row_count,
+          initial_id,
+          prefix
+        }),
+        callback
+      )
+
+      return
+    }
+
+    callback(null,{
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Save Printing Record: Input validation error`,
+        success:false
+      })
+    });
+
+  }catch(err){
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Unable to save printing record. Error: ${err} `,
+        success: false
+      })
+    })
+  }
+};
+
+module.exports = savePrintingRecords

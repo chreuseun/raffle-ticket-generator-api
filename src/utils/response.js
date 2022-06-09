5cd6758750157393e49ad0ecbb92c953c552124f
eunille <eunille.villegas@growsari.com>
2022-06-09 23:38:37 +0800

    Added createResponse & getRequestQueryStringParameters helper functions on src/utils/response.js

--- src/utils/response.js
+++ src/utils/response.js
@@ -31,6 +31,52 @@ const createResponseCallback = ({
   }
 };
 
+const createResponse = ({
+  statusCode = 500,
+  headers = {},
+  message = '',
+  data = null,
+  success = false
+}) => {
+  try{
+
+    return{
+      statusCode,
+      headers: {...DEFAULT_HTTP_HEADERS, ...headers},
+      body: JSON.stringify({
+        message, // message to display in front-end / client
+        success, // 200, 401, 401 504 .... 
+        data,
+      })
+    }
+  }catch{
+
+    return {
+      statusCode: 500,
+      headers: {...DEFAULT_HTTP_HEADERS, ...headers},
+      body: JSON.stringify({
+        message: err.message || 'Error is occured', 
+        success: false,
+        data,
+      })
+    }
+  }
+ 
+}
+
+const getRequestQueryStringParameters = ({event = {}}) => {
+  try{
+    const { queryStringParameters  = {} } = event
+
+    return queryStringParameters
+  }catch{
+
+    return {}
+  }
+}
+
 module.exports = {
-  createResponseCallback
+  createResponseCallback,
+  createResponse,
+  getRequestQueryStringParameters
 }
